27a9f3453e3501308b1e610f2032979f0a0c2e8c
VasLyber <lyberatosbill@hotmail.com>
2024-01-07 19:46:53 +0200

    added scatter plots

--- compost.py
+++ compost.py
@@ -2,85 +2,139 @@ import json
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, export_graphviz
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import LinearSVR
 from sklearn.preprocessing import StandardScaler
 import plotly.graph_objects as go
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.tree import plot_tree
 import numpy as np
+import subprocess
+import matplotlib.pyplot as plt
+from xgboost import XGBRegressor
 
 # Load CSV file using Pandas
 df = pd.read_csv('compost.csv') 
-
 df = df.replace(',', '.', regex=True)  
 
-# Extract features and target from the DataFrame
-features = df.iloc[:, [7,8,9,10,11]].values  
-target = df.iloc[:, [13,14,17,18] ].values 
-#[8,9,10,11,12]
-#[13,14,17,18] 
-#[12,13,14,15,16] 
+# Select input and output of the models
+# 3 different experiments
+
+# feed-duration
+# input = [1]
+# output = [14]
+
+# temperatures
+# input = [11]
+# output = [12,13]
+
+# percentage of feed
+input = [7,8,9,10]
+output = [11,12,17,18]
+
+features = df.iloc[:, input].values  
+target = df.iloc[:, output].values
+target_labels = df.columns[output] 
+
+# Collect the test set in order to have representative data
 selected_indices = [5, 10, 15, 20, 25 , 30, 35, 40]
-# Create a boolean mask for the selected indices
 mask = np.zeros(features.shape[0], dtype=bool)
 mask[selected_indices] = True
 
-# Use boolean indexing to get the selected and remaining rows
-x_train = features[mask]
-x_test = features[~mask]
+x_train = features[~mask]
+x_test = features[mask]
+
+target = target.astype(float)
+y_train = target[~mask]
+y_test = target[mask]
+
+# Standarise the input data
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-target = target.astype(float)
-y_train = target[mask]
-y_test = target[~mask]
-scaler = StandardScaler()
-y_train = scaler.fit_transform(y_train)
-y_test = scaler.transform(y_test)
+# Standarise the output data
+# scaler = StandardScaler()
+# y_train = scaler.fit_transform(y_train)
+# y_test = scaler.transform(y_test)
 
-# Define a list of regression models
+# Define regression models
 models = [
     LinearRegression(),
     KNeighborsRegressor(),
     DecisionTreeRegressor(),
-    MultiOutputRegressor(LinearSVR())
+    MultiOutputRegressor(LinearSVR()),
+    XGBRegressor()
 ]
 
-# Dictionary to store results
+flag = True
+
 results = {}
 
-# Loop through each model
 for model in models:
     model_name = type(model).__name__
     
     # Train the model
     model.fit(x_train, y_train)
+    
+    # Visualize the tree for Decision Tree
     if isinstance(model, DecisionTreeRegressor):
         # Visualize a limited-depth decision tree
         max_depth = 3  # Set the maximum depth you want to visualize
-        plt.figure(figsize=(18, 12))
-        plot_tree(model, filled=True, feature_names=df.columns[[7,8,9,10,11]], class_names=df.columns[[13,14,17,18] ], fontsize=10, max_depth=max_depth)
-        plt.title(f'{model_name} Decision Tree (Max Depth = {max_depth})')
-        image_file_path = f'./results/{model_name}_decision_tree.png'
-        plt.savefig(image_file_path)
-        print(f'Decision tree visualization saved to {image_file_path}')
+        
+        # Create a DOT format string for the decision tree
+        dot_data = export_graphviz(model, filled=True, feature_names=df.columns[input],
+                                    class_names=df.columns[output], max_depth=max_depth)
+        
+        # Save the DOT string to a file
+        dot_file_path = f'./results/{model_name}_decision_tree.dot'
+        with open(dot_file_path, 'w') as dot_file:
+            dot_file.write(dot_data)
+        
+        png_file_path = f'./results/{model_name}_decision_tree.png'
+        subprocess.run(['dot', '-Tpng', dot_file_path, '-o', png_file_path], check=True)
+   
+        print(f'Decision tree visualization saved to {png_file_path}')
 
-    
-    # Make predictions on the training set
+    # Make predictions on the training set and calculate metrics
     train_predictions = model.predict(x_train)
     train_mse = mean_squared_error(y_train, train_predictions)
     train_mae = mean_absolute_error(y_train, train_predictions)
     train_r2 = r2_score(y_train, train_predictions)
     
-    # Make predictions on the test set
+    # Make predictions on the test set and calculate metrics
     test_predictions = model.predict(x_test)
     test_mse = mean_squared_error(y_test, test_predictions)
     test_mae = mean_absolute_error(y_test, test_predictions)
     test_r2 = r2_score(y_test, test_predictions)
+
+    all_predictions = model.predict(features)
+    
+    if isinstance(model, XGBRegressor) and flag==True:
+        # Visualize the results
+        grid_size = (len(y_test[0]), 1)
+
+        fig, axes = plt.subplots(*grid_size, figsize=(15, 15))
+
+        axes = axes.flatten()
+
+        for i, ax in enumerate(axes[:len(y_test[0])]):
+            x_axis = [j for j in range(1, len(y_test) + 1)]  # Update x_axis for each subplot
+
+            # Scatter plot for actual values
+            ax.scatter(x_axis, y_test[:, i], color='black', label='Actual')
+
+            # Scatter plot for predicted values
+            ax.scatter(x_axis, test_predictions[:, i], color='red', label='Predicted')
+            ax.set_xlabel('Run')
+            ax.set_ylabel(target_labels[i])
+            ax.legend()
+            ax.tick_params(axis='x', rotation=45)
+
+        plt.suptitle('XGBoost Regression')
+        # Adjust layout and save the figure
+        plt.tight_layout(pad=3.0)
+        plt.savefig('./results/svr_plot.png')
 
     # Save predictions and ground truth to a CSV file
     predictions_df = pd.DataFrame({
@@ -91,7 +145,7 @@ for model in models:
     predictions_df.to_csv(f'./results/{model_name}_predictions.csv', index=False)
     print(f'Test predictions saved to {model_name}_predictions.csv')
     if hasattr(model, 'intercept_'):
-        print(model.intercept_)
+        print("Linear's Coefficients",model.coef_,model.intercept_)
     # Store results in the dictionary
     results[model_name] = {
         'train_mse': train_mse,
@@ -123,19 +177,19 @@ for model_name, result in results.items():
     if coefficients is not None:
         fig = go.Figure(data=go.Heatmap(
             z=coefficients,
-            x=df.columns[[7,8,9,10,11]],
-            y=df.columns[[13,14,17,18] ],
+            x=df.columns[input],
+            y=df.columns[output ],
             colorscale='YlOrRd',
             colorbar=dict(title='Coefficients')
         ))
 
         # Add annotations with coefficient values
-        for i in range(len(df.columns[[13,14,17,18] ])):
-            for j in range(len(df.columns[[7,8,9,10,11]])):
+        for i in range(len(df.columns[output])):
+            for j in range(len(df.columns[input])):
                 coefficient_value = float(coefficients[i][j])
                 fig.add_annotation(
-                    x=df.columns[[7,8,9,10,11]][j],
-                    y=df.columns[[13,14,17,18]][i],
+                    x=df.columns[input][j],
+                    y=df.columns[output][i],
                     text=f'{coefficient_value:.2f}',
                     showarrow=False,
                     font=dict(color='black', size=10)
